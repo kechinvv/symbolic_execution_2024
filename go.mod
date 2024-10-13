865cad6b99f4b7ffbae5a47cf283deb356066608
kechinvv <valeraghst@gmail.com>
2024-10-13 20:12:24 +0300

    soft_constraints

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 toolchain go1.22.8
 
-require golang.org/x/tools v0.26.0
-
-require github.com/kechinvv/go-z3 v0.0.0-20241012142128-dbca98fdf7bf
+require (
+	github.com/kechinvv/go-z3 v0.0.0-20241013133155-851effd3ad4d // indirect
+	golang.org/x/tools v0.26.0 // indirect
+)
--- testdata/tests/lab1/utils.go
+++ testdata/tests/lab1/utils.go
@@ -2,7 +2,10 @@ package lab1
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/kechinvv/go-z3/z3"
+	"github.com/kechinvv/symbolic_execution_2024/pkg"
 )
 
 const int_size = 64
@@ -22,6 +25,81 @@ func checkWithOutput(s *z3.Solver) {
 	println(s.AssertionsString())
 	if v, _ := s.Check(); !v {
 		fmt.Println("Unsolveable")
+	} else {
+		m := s.Model().String()
+		println(m)
+	}
+}
+
+func CheckWithUnsatCoreV0(s *z3.Solver) {
+	println(s.AssertionsString())
+	if v, _ := s.Check(); !v {
+		fmt.Println("unresolved")
+		unsatCore := s.UnsatCore()
+		for _, expr := range unsatCore {
+			println(expr.String())
+		}
+		for len(unsatCore) > 0 && !v {
+			soft_constraints := s.Assertions()
+			s.Pop()
+			s.Push()
+			for _, constr := range soft_constraints {
+				if !strings.HasPrefix(constr.String(),"(=> " + unsatCore[0].String()){
+					s.Assert(constr)  //PROBLEM - LOST TRACKED CONSTR
+				} 
+			}
+			println(s.AssertionsString())
+			if v, _ := s.Check(); !v {
+				println("unresolved")
+				unsatCore = s.UnsatCore()
+				for _, expr := range unsatCore {
+					println(expr.String())
+				}
+			} else {
+				m := s.Model().String()
+				println(m)
+				return
+			}
+		}
+	} else {
+		m := s.Model().String()
+		println(m)
+	}
+}
+
+
+func CheckWithUnsatCoreV1(s *z3.Solver, soft_constraints []pkg.Assumption) {
+	println(s.AssertionsString())
+	if v, _ := s.Check(); !v {
+		fmt.Println("unresolved")
+		unsatCore := s.UnsatCore()
+		for _, expr := range unsatCore {
+			println(expr.String())
+		}
+		for len(unsatCore) > 0 && !v {
+			s.Pop()
+			s.Push()
+			var active_constraints []pkg.Assumption
+			for _, constr := range soft_constraints {
+				if constr.Name.String() != unsatCore[0].String() {
+					s.AssertAndTrack(constr.Expr, constr.Name)
+					active_constraints = append(active_constraints, constr)
+				} 
+			}
+			soft_constraints = active_constraints
+			println(s.AssertionsString())
+			if v, _ := s.Check(); !v {
+				println("unresolved")
+				unsatCore = s.UnsatCore()
+				for _, expr := range unsatCore {
+					println(expr.String())
+				}
+			} else {
+				m := s.Model().String()
+				println(m)
+				return
+			}
+		}
 	} else {
 		m := s.Model().String()
 		println(m)
